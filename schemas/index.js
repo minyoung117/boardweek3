910e31c5abf4deca0b4faba2c669f9789b9d7f4e
soyi Jang <minyoung@soyiui-MacBookAir.local>
2022-10-06 23:08:38 +0900

    과제제출최종

--- schemas/index.js
+++ schemas/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connect = () => {
   mongoose
-    .connect('mongodb://localhost:27017/myboard')
+    .connect('mongodb+srv://test:sparta@cluster0.kelrm3b.mongodb.net/Cluster0?retryWrites=true')
     .catch(err => console.log(err))
 };
 
